b551dedddfa55d5b561129cf1823174ec177371a
Feith00 <ygorblack500@gmail.com>
2026-04-27 19:20:07 -0400

    Exercico1

---
+++ Exercico1.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+public class ItemForja
+{
+    public string nome;
+
+    public int tempS;
+
+    public string tipo;
+
+    public void forja()
+    {
+        
+    }
+}
