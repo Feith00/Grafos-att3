615cb8753c0ca6d9f20f5d0e39b5e77fd4026a18
Feith00 <ygorblack500@gmail.com>
2026-04-27 20:12:31 -0400

    Refactor ItemForja class and update forging logic

--- Exercico1.cs
+++ Exercico1.cs
@@ -3,14 +3,40 @@ using System.Collections.Generic;
 
 public class ItemForja
 {
-    public string nome;
+    public string Nome = "Ferro";
 
-    public int tempS;
+    public int TempS{get ; set;}
 
-    public string tipo;
+    public string Tipo = "Padrão";
+}
 
-    public void forja()
+public class Program
+{
+    
+    public static void Main()
     {
-        
+    Queue<ItemForja> forja = new Queue<ItemForja>();
+    bool verificarForja = false;
+
+    forja.Enqueue(new ItemForja{Nome = "Espada de Ferro" , TempS = 15 , Tipo = "Espada"});
+    forja.Enqueue(new ItemForja{Nome = "Escudo" , TempS = 10 , Tipo = "Escudo"});
+    forja.Enqueue(new ItemForja{Nome = "Pacote de Flechas" , TempS = 5 , Tipo = "Flecha"});
+
+    while (verificarForja == false)
+    {
+           if (forja.Count == 0) 
+        {
+               verificarForja = true;
+        }
+        ItemForja processando = forja.Dequeue();
+        Console.WriteLine($"Item fabricado com sucesso:{processando.Nome}!\n Tempo de Forja: {processando.TempS}\n");
+     
+
+        ItemForja proximo = forja.Peek();
+        Console.WriteLine($" Processando:{proximo.Nome}!\n Tempo Esperado {proximo.TempS}\n Tipo:{proximo.Tipo}\n");
+    }
+    Console.WriteLine("Fila Vazia nenhum item para forjar.");
     }
 }
+
+        
